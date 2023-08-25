7ca3f209469961e7ffc2346b773b058447671235
Sean <pca.sean.jeun@gmail.com>
2023-08-25 10:06:34 -0400

    able to click 'enter' button and trasition into new html elements to render a video

--- BMWExp/src/index.js
+++ BMWExp/src/index.js
@@ -14,7 +14,9 @@ function renderInitialView(){
     img2.src = 'Assets/volumeicon.png'
     img2.alt = 'volume icon'
     img2.setAttribute('id', 'volume-icon')
+    preMain.setAttribute('class', 'pre-main')
     heading.setAttribute('class', 'head')
+    enterButton.setAttribute('class', 'enter-button')
     volume.setAttribute('type', 'range')
     volume.setAttribute('min', '0')
     volume.setAttribute('max', '100')
@@ -23,13 +25,34 @@ function renderInitialView(){
 
     heading.textContent = 'Welcome to your BMW Ultimate Experience'
     warning.textContent = '⚠ Please take this time to adjust your volume before moving forward ⚠'
+    enterButton.textContent = 'ENTER'
     preMain.appendChild(heading)
+    preMain.appendChild(enterButton)
     preMain.appendChild(slider)
     slider.appendChild(volume)
     preMain.appendChild(img1)
     heading.appendChild(warning)
     warning.appendChild(img2)
 
+    enterButton.addEventListener('click', function () {
+        preMain.innerHTML = '';
+        ptsView(preMain);
+    });
+
+    function ptsView(container) {
+        const ptsview = document.createElement('div'); // Corrected tag name to 'div'
+        const video = document.createElement('video');
+        ptsview.setAttribute('class', 'pts-to-start'); // Corrected class name
+        video.setAttribute('class', 'background-vid');
+        video.src = "Assets/introvideo.mp4";
+        video.alt = "introduction video";
+
+        video.playbackRate = 0.7
+        video.autoplay = true;
+
+        ptsview.appendChild(video);
+        container.appendChild(ptsview); // Append ptsview to the container
+    }
 }
 
 
